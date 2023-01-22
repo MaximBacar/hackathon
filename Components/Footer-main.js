e1b0b0e4adaa5b7d600868b713496011a06cbfb2
Asmae Loulidi <70148929+aloulidi@users.noreply.github.com>
2023-01-22 02:51:35 -0500

    Create Footer-main.js

---
+++ Components/Footer-main.js
@@ -0,0 +1,79 @@
+import React from "react";
+import {
+Box,
+Container,
+Row,
+Column,
+FooterLink,
+Heading,
+TitleFooter
+} from "./FooterStyle";
+
+const Footer = () => {
+return (
+	<Box>
+	<h1 style={{ color: "white",
+				textAlign: "center",
+				marginTop: "-50px", fontFamily:"Serif" }}>COB - Cash Optimized Better
+        <p style={{ color:"white", textAlign:"center", fontSize: 16, width: 1000, marginLeft:"auto", marginRight:"auto"}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        
+	</h1>
+	<Container>
+		<Row>
+		<Column>
+			<Heading>About Us</Heading>
+			<FooterLink href="#">Aim</FooterLink>
+			<FooterLink href="#">Vision</FooterLink>
+			<FooterLink href="#">Testimonials</FooterLink>
+		</Column>
+		<Column>
+			<Heading>Services</Heading>
+			<FooterLink href="#">Writing</FooterLink>
+			<FooterLink href="#">Internships</FooterLink>
+			<FooterLink href="#">Coding</FooterLink>
+			<FooterLink href="#">Teaching</FooterLink>
+		</Column>
+		<Column>
+			<Heading>Contact Us</Heading>
+			<FooterLink href="#">Uttar Pradesh</FooterLink>
+			<FooterLink href="#">Ahemdabad</FooterLink>
+			<FooterLink href="#">Indore</FooterLink>
+			<FooterLink href="#">Mumbai</FooterLink>
+		</Column>
+		<Column>
+			<Heading>Social Media</Heading>
+			<FooterLink href="#">
+			<i className="fab fa-facebook-f">
+				<span style={{ marginLeft: "10px" }}>
+				Facebook
+				</span>
+			</i>
+			</FooterLink>
+			<FooterLink href="#">
+			<i className="fab fa-instagram">
+				<span style={{ marginLeft: "10px" }}>
+				Instagram
+				</span>
+			</i>
+			</FooterLink>
+			<FooterLink href="#">
+			<i className="fab fa-twitter">
+				<span style={{ marginLeft: "10px" }}>
+				Twitter
+				</span>
+			</i>
+			</FooterLink>
+			<FooterLink href="#">
+			<i className="fab fa-youtube">
+				<span style={{ marginLeft: "10px" }}>
+				Youtube
+				</span>
+			</i>
+			</FooterLink>
+		</Column>
+		</Row>
+	</Container>
+	</Box>
+);
+};
+export default Footer;
